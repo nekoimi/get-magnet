171ba381e3521cf294f1b9016a4669426eee2977
nekoimi <nekoimime@gmail.com>
2024-07-05 23:50:46 +0800

    添加中间件

--- admin/router.go
+++ admin/router.go
@@ -3,10 +3,13 @@ package admin
 import (
 	"github.com/gorilla/mux"
 	"github.com/nekoimi/get-magnet/admin/api/download"
+	"github.com/nekoimi/get-magnet/admin/middleware"
 )
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
+
+	r.Use(middleware.AuthMiddleware())
 
 	// 提交下载连接
 	r.HandleFunc("/api/v1/download/submit", download.Submit)
