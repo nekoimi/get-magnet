4fe83565159c4caa6e47f5fac0a8cde1977beb76
nekoimi <nekoimime@gmail.com>
2025-07-09 22:45:50 +0800

    feat：修改下载速度监控周期

--- internal/aria2/speed.go
+++ internal/aria2/speed.go
@@ -2,7 +2,23 @@ package aria2
 
 import (
 	log "github.com/sirupsen/logrus"
+	"time"
 )
+
+// LowSpeedNum 低速下载区间测速检查数量
+const LowSpeedNum = 10
+
+// LowSpeedTimeout 低速下载多长时间超时
+const LowSpeedTimeout = (LowSpeedNum + 5) * time.Minute
+
+// LowSpeedInterval 低速下载检查周期
+const LowSpeedInterval = 1 * time.Minute
+
+// LowSpeedCleanupInterval 低速下载记录清除检查周期
+const LowSpeedCleanupInterval = 5 * time.Minute
+
+// LowSpeedThreshold 速度小于100KB，speed 单位是 Bytes/s
+const LowSpeedThreshold = 102400
 
 type SpeedRecord struct {
 	Last []uint
