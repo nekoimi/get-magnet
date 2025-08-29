a00df67f8d2a95ea96dd0fa5718fe12265f96bc1
nekoimi <nekoimime@gmail.com>
2025-08-29 20:47:02 +0800

    测试页面Cloudflare人机验证

--- tests/local_test.go
+++ tests/local_test.go
@@ -2,22 +2,15 @@ package tests
 
 import (
 	"context"
-	"github.com/nekoimi/get-magnet/internal/bus"
 	"github.com/nekoimi/get-magnet/internal/config"
 	"github.com/nekoimi/get-magnet/internal/core"
-	"github.com/nekoimi/get-magnet/internal/crawler"
-	"github.com/nekoimi/get-magnet/internal/crawler/providers/javdb"
-	"github.com/nekoimi/get-magnet/internal/crawler/providers/sehuatang"
 	"github.com/nekoimi/get-magnet/internal/db"
-	"github.com/nekoimi/get-magnet/internal/downloader/aria2_downloader"
 	"github.com/nekoimi/get-magnet/internal/job"
 	"github.com/nekoimi/get-magnet/internal/logger"
 	"github.com/nekoimi/get-magnet/internal/pkg/rod_browser"
-	"github.com/nekoimi/get-magnet/internal/server"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"testing"
-	"time"
 )
 
 func Test_Run(t *testing.T) {
@@ -55,27 +48,42 @@ func Test_Run(t *testing.T) {
 	lc.Register(cronScheduler)
 	// RodBrowser
 	browser := rod_browser.NewRodBrowser(cfg.Browser)
-	lc.Register(browser)
-	// 下载器
-	downloadService := aria2_downloader.NewAria2DownloadService(cfg.Aria2, cronScheduler)
-	lc.Register(downloadService)
-	// 任务管理器
-	crawlerManager := crawler.NewCrawlerManager(cronScheduler)
-	crawlerManager.Register(javdb.NewJavDBCrawler(cfg.JavDB, browser))
-	crawlerManager.Register(javdb.NewJavDBActorCrawler(cfg.JavDB, browser))
-	crawlerManager.Register(sehuatang.NewSeHuaTangCrawler(browser))
-	// 任务处理引擎
-	engine := crawler.NewCrawlerEngine(cfg.Crawler, downloadService, crawlerManager)
-	lc.Register(engine)
-	// http服务
-	httpServer := server.NewHttpServer(cfg)
-	lc.Register(httpServer)
+	browser.Start(ctx)
+	//lc.Register(browser)
+	//// 下载器
+	//downloadService := aria2_downloader.NewAria2DownloadService(cfg.Aria2, cronScheduler)
+	//lc.Register(downloadService)
+	//// 任务管理器
+	//crawlerManager := crawler.NewCrawlerManager(cronScheduler)
+	//crawlerManager.Register(javdb.NewJavDBCrawler(cfg.JavDB, browser))
+	//crawlerManager.Register(javdb.NewJavDBActorCrawler(cfg.JavDB, browser))
+	//crawlerManager.Register(sehuatang.NewSeHuaTangCrawler(browser))
+	//// 任务处理引擎
+	//engine := crawler.NewCrawlerEngine(cfg.Crawler, downloadService, crawlerManager)
+	//lc.Register(engine)
+	//// http服务
+	//httpServer := server.NewHttpServer(cfg)
+	//lc.Register(httpServer)
+	//
+	//time.AfterFunc(30*time.Second, func() {
+	//	t.Log("提交测试任务...")
+	//	bus.Event().Publish(bus.SubmitJavDB.Topic(), "https://javdb.com/censored?vft=2&vst=1")
+	//})
+	//
+	//// StartAll and Waiting
+	//lc.StartAndWait()
 
-	time.AfterFunc(30*time.Second, func() {
-		t.Log("提交测试任务...")
-		bus.Event().Publish(bus.SubmitJavDB.Topic(), "https://javdb.com/login")
-	})
+	rawUrl := "https://rucaptcha.com/42"
+	page, closeFunc := browser.NewTabPage()
+	defer closeFunc(rawUrl)
+	page.MustNavigate(rawUrl)
+	// 等待页面加载
+	log.Debugf("等待页面 %s 加载...", rawUrl)
+	err := page.WaitLoad()
+	if err != nil {
+		panic(err)
+	}
+	log.Debugf("页面 %s 加载完毕...", rawUrl)
 
-	// StartAll and Waiting
-	lc.StartAndWait()
+	select {}
 }
